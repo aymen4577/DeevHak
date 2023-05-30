84af3efb6790bcefa50eef19fb594b8b47858c00
aymen4577 <aymenmannai989@gmail.com>
2023-05-30 11:34:53 +0100

    valid

--- front/src/app/components/Quiz.js
+++ front/src/app/components/Quiz.js
@@ -6,19 +6,27 @@ function Quiz() {
   return (
     <div>
       return (
+
     <div className="container mt-3">
       <div className="content-body">
-     
+      <div className="card">
+                            <div className="card-header">
+            
+                                <h1 className="mb-4">Enter the verification code</h1>
+                            </div>
+          <div className="card-body">
             <form >
-              <h3 className="form-title m-t0">Enter the verification code</h3>
+          
               <div className="row">
                 <div className="mb-3 col-md-6">
                   <input type="text" className="form-control" name="code"  />
+                  <br/>
                   <button className="btn btn-success">Verify</button>
                 </div>
               </div>
             </form>
-          
+          </div>
+          </div>
         
         <div className="form-group">
           <div className="alert alert-danger" role="alert">
