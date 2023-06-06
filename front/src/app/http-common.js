b49a2bf575eba7ae599ba1c6c371bc1e8f544ebc
aymen4577 <77078351+aymen4577@users.noreply.github.com>
2023-06-06 06:11:25 +0100

    Update http-common.js

--- front/src/app/http-common.js
+++ front/src/app/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://20.219.178.245:5000/api",
+  // 20.219.178.245
   headers: {
     "Content-Type": "multipart/form-data"
   }
